de59efa43b96f1d5802d72b866fb5bbfe83780dc
charit <charit@gmail.com>
2021-11-14 23:42:58 -0500

    Pre-processing data. Preview class information

--- preprocess.py
+++ preprocess.py
@@ -24,33 +24,49 @@ inputFile = 'train.csv'
 
 columns = ["name", "x1", "x2", "y1", "y2", "classname"]
 
-trainDF = pd.read_csv(os.path.join(dataFolder, inputFile),
-                      skiprows=1, names=columns)
+train_df = pd.read_csv(os.path.join(dataFolder, inputFile),
+                       skiprows=1, names=columns)
 
-iterator = trainDF.iterrows()
+iterator = train_df.iterrows()
 
-ouputCSVFile = []
+def preview_classes():
+    classes = []
+    preselectedClasses = {
+        "face_with_mask": 0,
+        "mask_colorful": 0,
+        "face_no_mask": 0,
+        "face_with_mask_incorrect": 0,
+        "mask_surgical": 0
+    }
+    for row_index in range(len(train_df)):
+        className = train_df[columns[5]][row_index]
+        if className not in classes:
+            classes.append(className)
+        if className in preselectedClasses.keys():
+            preselectedClasses[className] += 1
+    return classes, preselectedClasses
+
 
 preselectedClasses = [
     "face_with_mask",
     "mask_colorful",
     "face_no_mask",
-    "face_with_mask_incorrect",
     "mask_surgical",
 ]
 
-for rowIndex in range(len(trainDF)):
-    className = trainDF[columns[5]][rowIndex]
-    if className in preselectedClasses:
-        ouputCSVFile.append(rowIndex)
-
-
-def saveCSV(dirPath, fileName, rows):
+def saveCSV(dirPath, fileName):
     if not os.path.isdir(dirPath):
         os.makedirs(dirPath)
-    df = trainDF.iloc[rows, :]
+
+    selectedClassIndexes = []
+    for row_index in range(len(train_df)):
+        className = train_df[columns[5]][row_index]
+        if className in preselectedClasses:
+            selectedClassIndexes.append(row_index)
+
+    df = train_df.iloc[selectedClassIndexes, :]
     df.to_csv(os.path.join(dirPath, fileName))
     print(f'Saved file {fileName} successfully!')
 
 
-saveCSV(os.path.join(rootDir, 'preprocessed'), "data.csv", ouputCSVFile)
+saveCSV(os.path.join(rootDir, 'preprocessed'), "data.csv")
